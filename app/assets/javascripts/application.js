2b52f608b6cfc7e18ab51c5c97ab63cbf212e420
hocuspocus2015 <alicesuckling@live.co.uk>
2016-01-19 17:12:10 +0000

    update devise_helper

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,10 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-$("form#sign_up_user").bind "ajax:success", (e, data, status, xhr) ->
-    if data.success
-      $('#sign_up').modal('hide')
-      $('#sign_up_button').hide()
-      $('#submit_comment').slideToggle(1000, "easeOutBack" )
-    else
-      alert('failure!')
