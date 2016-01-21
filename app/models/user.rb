6a9cc1991dda03a81c1caa6038645285576604c4
hocuspocus2015 <alicesuckling@live.co.uk>
2016-01-21 17:06:44 +0000

    improve user experience and solve bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
  
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable #:confirmable
          
   has_many :lists, dependent: :destroy
          
